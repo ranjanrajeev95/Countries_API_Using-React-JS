08d4b95126362e33e6cdf40effef310386124b6c
Rajeev Ranjan <79941632+ranjanrajeev95@users.noreply.github.com>
2021-08-31 00:48:46 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+React task - REST Countries API with color theme switcher. Integrated with the [REST Countries API](https://restcountries.eu) to pull country data and display it like in the designs.
+
+Users should be able to:
+
+- See all countries from the API on the homepage
+- Search for a country using an `input` field
+- Filter countries by region
+- Click on a country to see more detailed information on a separate page
+- Click through to the border countries on the detail page
+- Toggle the color scheme between light and dark mode
+
+You will find mobile and desktop designs in light and dark mode color schemes for both pages.
---
+++ precache-manifest.ca56e0528570295769581f7773bb612d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a552a7cc1b9a899838c55120a664c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b8e3d6e8a811e909c2",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8c5f7a34b586e9d76b8f",
+    "url": "/static/css/main.991c7543.chunk.css"
+  },
+  {
+    "revision": "19b8e3d6e8a811e909c2",
+    "url": "/static/js/2.155885fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.155885fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5f7a34b586e9d76b8f",
+    "url": "/static/js/main.cd13cd80.chunk.js"
+  },
+  {
+    "revision": "f4e3d70bd5967adc144d",
+    "url": "/static/js/runtime-main.a5f11eaf.js"
+  }
+]);
